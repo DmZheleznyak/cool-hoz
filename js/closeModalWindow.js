183279042a408c1d5c505fceffac0c07c40fdaf3
DmZheleznyak <11lione1991@gmail.com>
2020-04-16 02:16:13 +0300

    adding example cool-hoz

---
+++ js/closeModalWindow.js
@@ -0,0 +1,5 @@
+function btnClosePopup() {
+    var modalWindow = document.getElementsByClassName('modal-window')[0]
+    modalWindow.classList.remove('modal-window')
+    modalWindow.classList.add('modal-window-invisible')
+}
