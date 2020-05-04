be266c3d2de69fb6aa30a0abb451503b0887ae4a
DmZheleznyak <11lione1991@gmail.com>
2020-05-05 00:20:54 +0300

    add button hamburger for phone screen

---
+++ js/buttonHamburger.js
@@ -0,0 +1,15 @@
+var btnHamburger = document.getElementById('button-hamburger')
+var navListServices = document.getElementById('navlist-services')
+
+btnHamburger.addEventListener('click', function() {
+    if ( !this.classList.contains('button-hamburger_open') ) {
+        this.classList.add('button-hamburger_open')
+        navListServices.classList.add('navlist-services_open')
+        return    
+    } 
+    if ( this.classList.contains('button-hamburger_open') ) {
+        this.classList.remove('button-hamburger_open')
+        navListServices.classList.remove('navlist-services_open')
+        return    
+    }   
+})
