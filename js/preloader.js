30f799b4a65a36d07c3d2cbdf909119ab27ec748
DmZheleznyak <11lione1991@gmail.com>
2020-07-06 18:24:43 +0300

    change preload, add text for service detailing

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const preloadContainer = document.getElementById('preload');
-    // debugger;
+    
     preloadContainer.classList.remove('preload');
     preloadContainer.classList.add('preload-none');
 }
